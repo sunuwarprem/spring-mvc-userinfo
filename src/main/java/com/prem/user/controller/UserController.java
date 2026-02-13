e23e02fb85033c73e19f216094089eb5f553f7ca
prem sunuwar <rhel8.prem@gmail.com>
2026-02-12 21:21:44 -0500

    add comment

--- src/main/java/com/prem/user/controller/UserController.java
+++ src/main/java/com/prem/user/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class userController {
+public class UserController {
 
     @InitBinder
     public void initBinder(WebDataBinder webDataBinder){
